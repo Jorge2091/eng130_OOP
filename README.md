7d5f3829bd5699c29e8b60dc019313a9933191c3
Jorge2091 <93310766+Jorge2091@users.noreply.github.com>
2022-10-13 12:33:10 +0100

    import lesson

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ### Methods/functions
 #### Modules
 ##### Lib - Packages
+
+#### Use case - benefits - examples of builtin modules - packages
+- Lucky draw, inside python modules, we have one called random. Everytime you print something using this module, it will give you a random number between 0 and 1.
+```python
+import random
+
+print(random.random())
+```
---
+++ oop_modules_packages.py
@@ -0,0 +1,19 @@
+# Lucky draw
+# key word called import
+# import random
+#
+# print(random.random())
+#each  time it's run, it generates a random number
+
+from random import random
+import math
+
+print(random())
+# calculate DOB or year of birth
+
+number = 23.66
+# any number - to round the figure up
+# number .50 above to round up
+# number 1.49 or less round down to 1
+print(math.ceil(number)) # ceil will round the figure up
+print(math.ceil(number)) # floor will help you round the figure to bottom
